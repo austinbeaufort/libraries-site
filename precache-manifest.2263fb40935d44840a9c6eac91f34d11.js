5a88e0d307cb393e5da761186318a4c22e4d4702
austinbeaufort <austinbeaufort7@gmail.com>
2019-08-27 21:35:21 -0400

    Updates

---
+++ precache-manifest.2263fb40935d44840a9c6eac91f34d11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c84cf5fa172d63bd895ba8fe7a3c70c",
+    "url": "/libraries-site/index.html"
+  },
+  {
+    "revision": "ab25622af711210d3231",
+    "url": "/libraries-site/static/css/main.ab30996a.chunk.css"
+  },
+  {
+    "revision": "0881664b8fcb2aac3ad0",
+    "url": "/libraries-site/static/js/2.2033c715.chunk.js"
+  },
+  {
+    "revision": "ab25622af711210d3231",
+    "url": "/libraries-site/static/js/main.1ba05386.chunk.js"
+  },
+  {
+    "revision": "5435ec4ce576b6126822",
+    "url": "/libraries-site/static/js/runtime~main.0a29603e.js"
+  }
+]);
